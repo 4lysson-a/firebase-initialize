702e5dddb50641a7ff6ab76b48dd5552ece3b0a8
4lysson-a <4ly.alcantara@gmail.com>
2022-04-20 19:56:40 -0300

    feat: inprove start script

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 FILE=.env
+ENVD_DIST_URL=https://gist.githubusercontent.com/4lysson-a/368e0f9e7a9bf49213c9d905956f141d/raw/16d839a14ae0797d512e42934e98a3520678b561/.env.dist
 
 copy_env(){
   if test -f ".env.dist"; then
     echo "Criando arquivo .env" 
     sudo cp .env.dist .env
+    rm .env.dist
+    wget "$ENVD_DIST_URL"
   fi 
 }
 
